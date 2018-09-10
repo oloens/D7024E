1148e63e5fb54b62bae2937ab484c8307f0ff8c1
Olof Enström <oloens-4@student.ltu.se>
2018-09-10 16:02:57 +0200

    initial commit

---
+++ docker/app.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+
+//just testing deployment of go in a docker container, and subsequently
+// deploying a cluster using docker swarm
+func main() {
+	for {
+	fmt.Println("testing hello world thing")
+	}
+}
