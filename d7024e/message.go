fbb6d6cb03436c0a10b468db15372c98b2ae58c9
jonolo-3 <jonathaan.olsson@gmail.com>
2018-09-20 11:26:41 +0200

    update proto

--- d7024e/message.go
+++ d7024e/message.go
@@ -1,7 +1,11 @@
 package d7024e
 
 import (
+	fmt "fmt"
 	"sync"
+
+	pb "../protobuf"
+	proto "github.com/golang/protobuf/proto"
 )
 
 type Message struct {
@@ -10,5 +14,20 @@ type Message struct {
 }
 
 func (this *Message) handleMsg(channel chan []byte, me *Contact, network *Network) {
+	data := <-channel
+	message := &pb.KMessage{}
+	err := proto.Unmarshal(data, message)
+	if err != nil {
+		fmt.Println(err)
+	}
+	sndr := NewContact(NewKademliaID(message.GetSndrID()), message.GetSndrAddress())
+	this.network //UPDATE routingtable for sender, need some kind of UpdateRoutingTable(sender) func
 
+	switch message.GetMsgType() {
+	//Cases
+	}
+}
+
+func buildMsg(input []string) *pb.KMessage {
+	return
 }
